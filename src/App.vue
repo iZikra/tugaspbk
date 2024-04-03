f0d3ecb2b90bb3c7a682cc210f0b04758d5dd4ec
iZikra <zikrallahalhasy@student.uir.ac.id>
2024-04-03 23:19:04 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <header>
+        <div class="head">
+            <h1>Zikrallah Al Hady</h1>
+            <nav>
+                <ul>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <section id="about" class="section">
+        <div class="aboutme">
+            <h2>About Me</h2>
+            <p>nama saya adalah Zikrallah Al Hady, saya lahir dipekanbaru. saya lulusan dari smk 2 pekanbaru</p>
+        </div>
+        <img src="./assets/aa.jpg" alt="hehe" class="gambar">
+    </section>
+
+    <section id="contact" class="section">
+        <div class="skill">
+            <h2>Skill</h2>
+            <div class="portfolio-filter">
+                <button data-filter="all" class="active">All</button>
+                <button data-filter="web">Web Development</button>
+                <button data-filter="design">Design</button>
+                <button data-filter="writing">Remap ecu</button>
+            </div>
+        </div>
+    </section>
+
+    <section id="contact" class="section">
+        <div class="container">
+            <h2>Pengalaman</h2>
+            <p>ini adalah marut yaitu suatu aplikasi untuk mencari jasa masseur.ini adalah project terakhir saya bersama rekan-rekan saya</p>
+            <img src="./assets/1.jpg" alt="hehe" class="gambar">
+            <img src="./assets/2.jpg" alt="hehe" class="gambar">
+            <img src="./assets/3.jpg" alt="hehe" class="gambar">
+        </div>
+    </section>
+
+    <footer>
+        <div class="futer">
+            <p>&copy; ZIKRALLAH AL HADY.</p>
+        </div>
+    </footer>
+
+</template>
